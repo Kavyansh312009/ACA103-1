91ab1febef395003fd5176eb5932f78a39239553
Kavyansh312009 <94540332+Kavyansh312009@users.noreply.github.com>
2022-04-07 15:37:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+https://teachablemachine.withgoogle.com/models/wcoEO1Pyc/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
+Webcam.attach('camera');
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+      document.getElementById("captured_image").src = data_uri;  
+    });
+}
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/wcoEO1Pyc/.model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!!!');
+}
